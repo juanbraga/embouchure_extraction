4e9a56e401c426e3cff9a6b9aa97f98031731b16
jbraga@dell <juanbragabrum@gmail.com>
2016-12-09 17:21:33 -0300

    giving it all

--- informe2/results_visualization.py
+++ informe2/results_visualization.py
@@ -7,87 +7,229 @@ Created on Mon Dec 05 20:22:02 2016
 
 import numpy as np
 import seaborn as sns
+sns.set(style="whitegrid")
 import matplotlib.pyplot as plt
-import pandas
+import pandas as pd
 
 plt.close('all')
 
-mfcc_20_40 = np.array([79,71,72,76,74,73,77,80,79,77,72,70,82,81,82])
-mfcc_30_40 = np.array([82,72,77,79,78,75,80,82,80,80,75,76,84,84,82])
-mfcc_40_40 = np.array([82,74,77,81,79,76,81,82,80,82,75,77,85,83,83])
+#%% CRAZY PLOTS
 
-d_mfcc = {'mfcc_20_40': mfcc_20_40, 'mfcc_30_40': mfcc_30_40, 'mfcc_40_40': mfcc_40_40}
-df_mfcc = pandas.DataFrame(data=d_mfcc)
+##%% pairwaise comparison classical features
+#features_spectral="../features/" + 'juan' + "_spectral_" + str(256) + str(128) + "_train.npy"
+#data_spectral=np.load(features_spectral)
+#d_spectral = {'rolloff': data_spectral[0,:], 'centroid': data_spectral[1,:], 'bandwith': data_spectral[2,:], 'zcr': data_spectral[3,:], 'voicing': data_spectral[4,:], 'gt': data_spectral[5,:]}
+#df_spectral = pd.DataFrame(data=d_spectral)
+#sns.pairplot(df_spectral, hue='gt', vars=['voicing', 'zcr','centroid'])
+#
+##%% 2x2 probabilites out of RandomForest
+#melcoeff=20
+#melbands=40
+#probabilties="../prediction/" + 'pablo' + "_mfcc_" + str(melcoeff) + str(melbands) + "_proba.npy"
+#proba=np.load(probabilties)
+#proba=proba.T
+#d_proba = {'bhc': proba[0,:], 'breathy': proba[1,:], 'normal': proba[2,:], 'time': proba[3,:], 'gt': proba[4,:]}
+#df_proba = pd.DataFrame(data=d_proba)
+#sns.pairplot(df_proba, hue='gt', vars=['bhc', 'breathy','normal'])
 
-speccont_3 = np.array([64,63,65,68,65,73,73,71,75,60,60,62,70,69,71])
-speccont_6 = np.array([66,71,67,77,77,75,78,77,77,70,68,70,78,77,80])
+#%% LPC
 
-d_speccon = {'speccont_3': speccont_3, 'speccont_6': speccont_6}
-df_speccon = pandas.DataFrame(data=d_speccon)
+lpc_10_knn = [47,52,55,42,55]
+lpc_10_rf = [43,56,57,40,54]
+lpc_10_svm = [50,70,70,56,72]
+lpc_20_knn = [47,52,51,46,52]
+lpc_20_rf = [45,52,51,44,52]
+lpc_20_svm = [52,72,70,45,64]
+lpc_40_knn = [49,52,56,53,56]
+lpc_40_rf = [45,52,54,45,51]
+lpc_40_svm = [55,69,69,48,67]
 
-spectral_256128 = np.array([70,69,72,74,74,69,78,77,77,71,70,70,77,77,74])
-spectral_1024512 = np.array([69,68,74,76,75,70,80,80,79,72,71,73,78,77,75])
-spectral_20481024 = np.array([70,68,74,76,76,70,81,80,80,71,71,73,78,77,75])
+d_lpc = {'accuracy' : pd.Series(np.concatenate((lpc_10_knn,lpc_10_rf,lpc_10_svm,lpc_20_knn,lpc_20_rf,lpc_20_svm,lpc_40_knn,lpc_40_rf,lpc_40_svm))),
+     'artist': pd.Series(['ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire']),
+     'clf': pd.Series(['knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm']),
+     'poles': pd.Series(['10poles','10poles','10poles','10poles','10poles','10poles','10poles','10poles','10poles','10poles','10poles','10poles', \
+     '10poles','10poles','10poles','20poles','20poles','20poles','20poles','20poles','20poles','20poles','20poles','20poles','20poles','20poles', \
+     '20poles','20poles','20poles','20poles','40poles','40poles','40poles','40poles','40poles','40poles','40poles','40poles','40poles','40poles', \
+     '40poles','40poles','40poles','40poles','40poles'])}
 
-d_spectral = {'spectral_256128': spectral_256128, 'spectral_1024512': spectral_1024512, 'spectral_20481024': spectral_20481024}
-df_spectral = pandas.DataFrame(data=d_spectral)
 
-lpc_10 = np.array([47,43,50,52,56,70,55,57,70,42,40,56,55,54,72])
-lpc_20 = np.array([47,45,52,52,52,72,51,51,70,46,44,45,52,52,64])
-lpc_40 = np.array([49,45,55,52,52,69,56,54,69,53,45,48,56,51,67])
 
-d_lpc = {'lpc_10': lpc_10, 'lpc_20': lpc_20, 'lpc_40': lpc_40}
-df_lpc = pandas.DataFrame(data=d_lpc)
+df_lpc = pd.DataFrame(data=d_lpc)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="poles", data=df_lpc,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
 
-plt.figure()
-plt.subplot(1,4,1)
-sns.boxplot(data=df_mfcc)
-plt.ylim( (35, 90) )
-plt.subplot(1,4,2)
-sns.boxplot(data=df_spectral)
-plt.ylim( (35, 90) )
-plt.subplot(1,4,3)
-sns.boxplot(data=df_speccon)
-plt.ylim( (35, 90) )
-plt.subplot(1,4,4)
-sns.boxplot(data=df_lpc)
-plt.ylim( (35, 90) )
+#%% MFCC
+
+mfcc_2040_knn = [79,76,77,77,82]
+mfcc_2040_rf = [71,74,80,72,81]
+mfcc_2040_svm = [72,73,79,70,82]
+mfcc_3040_knn = [72,79,80,80,84]
+mfcc_3040_rf = [72,78,82,75,84]
+mfcc_3040_svm = [77,75,80,76,82]
+mfcc_4040_knn = [82,81,81,82,85]
+mfcc_4040_rf = [74,79,82,75,83]
+mfcc_4040_svm = [77,76,80,77,83]
+
+d_mfcc = {'accuracy' : pd.Series(np.concatenate((mfcc_2040_knn,mfcc_2040_rf,mfcc_2040_svm,mfcc_3040_knn,mfcc_3040_rf,mfcc_3040_svm,mfcc_4040_knn,mfcc_4040_rf,mfcc_4040_svm))),
+     'artist': pd.Series(['ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire']),
+     'clf': pd.Series(['knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm']),
+     'params': pd.Series(['20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands', \
+     '30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands','30coeff-40bands',\
+     '30coeff-40bands','30coeff-40bands','30coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands', \
+     '40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands',\
+     '40coeff-40bands','40coeff-40bands','40coeff-40bands','40coeff-40bands'])}
+
+
+
+df_mfcc = pd.DataFrame(data=d_mfcc)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="params", data=df_mfcc,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="artist", y="accuracy", hue="params", data=df_mfcc,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+
+#%% SPECTRAL
+
+spectral_256128_knn = [70,76,81,71,78]
+spectral_256128_rf = [68,76,80,71,77]
+spectral_256128_svm = [74,70,80,73,75]
+spectral_1024512_knn = [69,76,80,72,78]
+spectral_1024512_rf = [68,75,80,71,77]
+spectral_1024512_svm = [74,70,79,73,75]
+spectral_20481024_knn = [70,74,78,71,77]
+spectral_20481024_rf = [69,74,77,70,77]
+spectral_20481024_svm = [72,69,77,70,74]
+
+d_spectral = {'accuracy' : pd.Series(np.concatenate((spectral_256128_knn,spectral_256128_rf,spectral_256128_svm,spectral_1024512_knn,spectral_1024512_rf,spectral_1024512_svm,spectral_20481024_knn,spectral_20481024_rf,spectral_20481024_svm))),
+     'artist': pd.Series(['ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire']),
+     'clf': pd.Series(['knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm']),
+     'params': pd.Series(['256win-128hop','256win-128hop','256win-128hop','256win-128hop','256win-128hop','256win-128hop', \
+     '256win-128hop','256win-128hop','256win-128hop','256win-128hop','256win-128hop','256win-128hop','256win-128hop', \
+     '256win-128hop','256win-128hop','1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop', \
+     '1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop','1024win-512hop',\
+     '1024win-512hop','1024win-512hop','1024win-512hop','2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop', \
+     '2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop',\
+     '2048win-256hop','2048win-256hop','2048win-256hop','2048win-256hop'])}
+
+
+df_spectral = pd.DataFrame(data=d_spectral)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="params", data=df_spectral,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+#%% SPECTRAL CONTRAST
+
+spectralcontrast_6_knn = [66,77,78,70,78]
+spectralcontrast_6_rf = [71,77,77,68,77]
+spectralcontrast_6_svm = [67,75,77,70,80]
+spectralcontrast_3_knn = [64,68,73,60,70]
+spectralcontrast_3_rf = [63,65,71,60,69]
+spectralcontrast_3_svm = [65,73,75,62,71]
+
+d_spectralcontrast = {'accuracy' : pd.Series(np.concatenate((spectralcontrast_3_knn,spectralcontrast_3_rf,spectralcontrast_3_svm,spectralcontrast_6_knn,spectralcontrast_6_rf,spectralcontrast_6_svm))),
+     'artist': pd.Series(['ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire', \
+     'ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire','ulla','pablo','emma','juan','claire']),
+     'clf': pd.Series(['knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm', \
+     'knn','knn','knn','knn','knn','rf','rf','rf','rf','rf','svm','svm','svm','svm','svm']),
+     'bands': pd.Series(['3bands','3bands','3bands','3bands','3bands','3bands', \
+     '3bands','3bands','3bands','3bands','3bands','3bands','3bands', \
+     '3bands','3bands','6bands','6bands','6bands','6bands','6bands', \
+     '6bands','6bands','6bands','6bands','6bands','6bands','6bands',\
+     '6bands','6bands','6bands'])}
+
+
+df_spectralcontrast = pd.DataFrame(data=d_spectralcontrast)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="bands", data=df_spectralcontrast,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="artist", y="accuracy", hue="bands", data=df_spectralcontrast,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+#%% BHC Vs. Breathy
 
 mfcc_bhcbr = np.array([80,72,74,78,74,75,76,76,77,78,72,64,82,82,82])
 speccont_bhcbr = np.array([67,70,57,78,76,78,77,77,78,69,68,64,77,77,79])
 
-d_bhcbr = {'mfcc_bhcbr': mfcc_bhcbr, 'speccont_bhcbr': speccont_bhcbr}
-df_bhcbr = pandas.DataFrame(data=d_bhcbr)
-
-plt.figure()
-sns.boxplot(data=df_bhcbr)
-plt.ylim( (50, 90) )
-
-mfccvoicing = np.array([80,72,74,78,74,75,76,76,77,78,72,64,82,82,82])
-
-d_plus_voicing = {'mfcc_20_40': mfcc_20_40, 'mfccvoicing': mfccvoicing}
-df_plus_voicing = pandas.DataFrame(data=d_plus_voicing)
-
-plt.figure()
-sns.boxplot(data=df_plus_voicing)
-plt.ylim( (50, 90) )
-
-#%%
-features_spectral="../features/" + 'juan' + "_spectral_" + str(256) + str(128) + "_train.npy"
-data_spectral=np.load(features_spectral)
-d_spectral = {'rolloff': data_spectral[0,:], 'centroid': data_spectral[1,:], 'bandwith': data_spectral[2,:], 'zcr': data_spectral[3,:], 'voicing': data_spectral[4,:], 'gt': data_spectral[5,:]}
-df_spectral = pandas.DataFrame(data=d_spectral)
-sns.pairplot(df_spectral, hue='gt', vars=['voicing', 'zcr','centroid'])
-
-#%%
-melcoeff=20
-melbands=40
-probabilties="../prediction/" + 'pablo' + "_mfcc_" + str(melcoeff) + str(melbands) + "_proba.npy"
-proba=np.load(probabilties)
-proba=proba.T
-d_proba = {'bhc': proba[0,:], 'breathy': proba[1,:], 'normal': proba[2,:], 'time': proba[3,:], 'gt': proba[4,:]}
-df_proba = pandas.DataFrame(data=d_proba)
-sns.pairplot(df_proba, hue='gt', vars=['bhc', 'breathy','normal'])
+d_bhcbr = {'accuracy' : pd.Series(np.concatenate((mfcc_bhcbr,speccont_bhcbr))),
+     'artist': pd.Series(['ulla','ulla','ulla','pablo','pablo','pablo','emma','emma','emma','juan','juan','juan','claire','claire','claire', \
+     'ulla','ulla','ulla','pablo','pablo','pablo','emma','emma','emma','juan','juan','juan','claire','claire','claire']),
+     'clf': pd.Series(['knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm', \
+     'knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm',]),
+     'params': pd.Series(['20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','6bands','6bands','6bands','6bands','6bands', \
+     '6bands','6bands','6bands','6bands','6bands','6bands','6bands',\
+     '6bands','6bands','6bands'])}
 
 
+df_bhcbr = pd.DataFrame(data=d_bhcbr)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="params", data=df_bhcbr,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="artist", y="accuracy", hue="params", data=df_bhcbr,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+#%% MFCC + Voicing
+
+mfcc_2040 = np.array([79,71,72,76,74,73,77,80,79,77,72,70,82,81,82])
+mfccvoicing = np.array([80,74,74,77,76,76,78,81,80,77,74,71,83,82,82])
+
+d_mfccvoicing = {'accuracy' : pd.Series(np.concatenate((mfcc_2040,mfccvoicing))),
+     'artist': pd.Series(['ulla','ulla','ulla','pablo','pablo','pablo','emma','emma','emma','juan','juan','juan','claire','claire','claire', \
+     'ulla','ulla','ulla','pablo','pablo','pablo','emma','emma','emma','juan','juan','juan','claire','claire','claire']),
+     'clf': pd.Series(['knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm', \
+     'knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm','knn','rf','svm',]),
+     'params': pd.Series(['20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands','20coeff-40bands', \
+     '20coeff-40bands','20coeff-40bands','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing', \
+     '20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing', \
+     '20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing','20coeff-40bands + Voicing'])}
+
+
+df_mfccvoicing = pd.DataFrame(data=d_mfccvoicing)
+     
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="clf", y="accuracy", hue="params", data=df_mfccvoicing,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
+
+# Draw a pointplot to show pulse as a function of three categorical factors
+g = sns.factorplot(x="artist", y="accuracy", hue="params", data=df_mfccvoicing,
+                   capsize=.2, palette="hls", size=6, aspect=.75)
+g.despine(left=True)
 
